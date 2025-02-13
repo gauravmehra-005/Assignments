931f4eed72cd31c2e7702e35babb4a72be77aca6
gauravmehra-005 <gaurav.mehra@wissen.com>
2025-02-14 01:34:02 +0530

    Testing webhooks in Calculator

--- calculator/src/main/java/calculator/Calculator.java
+++ calculator/src/main/java/calculator/Calculator.java
@@ -1,4 +1,5 @@
 package calculator;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
@@ -10,5 +11,25 @@ public class Calculator {
 
     public int mul(int a, int b) {
         return a * b;
+    }
+
+    public int div(int a, int b) throws ArithmeticException{
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+    
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        int sum = calc.add(10, 5);
+        int difference = calc.sub(10, 5);
+        int product = calc.mul(10, 5);
+        int quotient = calc.div(10, 5);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
     }
 }
